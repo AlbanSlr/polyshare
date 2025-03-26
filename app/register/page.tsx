9fd0c97171af2639f13a673950453809a1d1fd0e
AlbanZ <47938712+AlbanZ@users.noreply.github.com>
2025-03-26 13:27:28 +0100

    petit fix

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,6 +1,13 @@
 import { RegisterForm } from "@/components/register-form"
+import { getSession } from "@/lib/session";
+import { redirect } from "next/navigation";
 
-export default function Register() {
+export default async function Register() {
+  const session = await getSession();
+  
+    if (session.user?.isLoggedIn) {
+      redirect("/home");
+    }
     return (
         <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
           <div className="w-full max-w-sm">
